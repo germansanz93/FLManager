f22ee0e4e7d947d47abebc706c0b675964da7141
germansanz93 <germansanz93@gmail.com>
2021-12-02 00:03:00 -0300

    Feedlot service created with spring initializer

---
+++ feedlot-service/src/main/java/com/flmanager/feedlotservice/FeedlotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flmanager.feedlotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedlotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedlotServiceApplication.class, args);
+	}
+
+}
