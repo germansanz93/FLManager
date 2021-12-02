c33c91d17956d2e5f1c77880d1e8b7478325464d
German Sanz <64764636+germansanz93@users.noreply.github.com>
2021-12-02 00:14:38 -0300

    Project init
    * Feedlot service created with spring initializer
    
    * README updated
    
    * edited .gitignore
    
    * Delete MavenWrapperDownloader.java
    
    * Delete mvnw
    
    * Delete mvnw.cmd
    
    * Delete maven-wrapper.properties
    
    * Delete feedlot-service/.mvn/wrapper directory

---
+++ feedlot-service/src/test/java/com/flmanager/feedlotservice/FeedlotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flmanager.feedlotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedlotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
